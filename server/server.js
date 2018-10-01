725df4844a5f79e7bb929ab4248008576def9f94
Arkajit Mondal <arkajit.18@gmail.com>
2018-10-01 12:46:31 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -20,6 +20,14 @@ app.post('/todos', (request, response) => {
     });
 });
 
+app.get('/todos', (request, response) => {
+    Todo.find().then((todos) => {
+        response.send({todos});
+    }, (err) => {
+        response.status(400).send(err);
+    })
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
