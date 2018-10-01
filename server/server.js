95d840da90843e63e5750bdd40a2b95228800664
Arkajit Mondal <arkajit.18@gmail.com>
2018-10-01 17:07:59 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
+const {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
-var User = require('./models/user');
+var {User} = require('./models/user');
 
 var app = express();
 
@@ -25,6 +27,22 @@ app.get('/todos', (request, response) => {
         response.send({todos});
     }, (err) => {
         response.status(400).send(err);
+    });
+});
+
+//Get /todos/1234321
+app.get('/todos/:id', (request, response) => {
+    var id = request.params.id;
+    if (!ObjectID.isValid(id)) {
+        response.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            response.status(404).send();
+        }
+        response.send({todo});
+    }, (error) => {
+        response.status(400).send();
     });
 });
 
