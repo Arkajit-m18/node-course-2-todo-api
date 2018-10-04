3d88fb3cc1f7990df61bcfda4ab88dda38b13d9d
Arkajit Mondal <arkajit.18@gmail.com>
2018-10-04 18:07:30 +0530

    Add tests for POST/users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -264,3 +264,53 @@ describe('POST /users', () => {
             .end(done);
     });
 });
+
+describe('POST /users/login', () => {
+
+    it ('should login user and return auth token', (done) => {
+        var email = users[1].email;
+        var password = users[1].password;
+        request(app)
+            .post('/users/login')
+            .send({email, password})
+            .expect(200)
+            .expect((response) => {
+                expect(response.headers['x-auth']).toBeTruthy();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens[0]).toMatchObject({
+                        access: 'auth',
+                        token: res.headers['x-auth']
+                    });
+                    done();
+                })
+                .catch((e) => done(e));
+            });
+    });
+
+    it ('should reject invalid login', (done) => {
+        var email = 'abc';
+        var password = '123';
+        request(app)
+            .post('/users/login')
+            .send({email, password})
+            .expect(400)
+            .expect((response) => {
+                expect(response.headers['x-auth']).not.toBeTruthy();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                })
+                .catch((e) => done(e));
+            });
+    });
+});
